efba99346ba717245224b16d404f064e881cbb47
Hasan Shaikh <35498957+shasan419@users.noreply.github.com>
2025-04-10 12:24:26 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,15 @@
+// const heading = React.createElement("h1", {id:"heading"}, "Hello World from React!");
+const Parent = React.createElement("div", {id:"parent"}, [
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1", {}, "Hello World from React!"),
+        React.createElement("h2", {}, "Hello World from React!"),
+    ]),
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1", {}, "Hello World from React!"),
+        React.createElement("h2", {}, "Hello World from React!"),
+    ])
+])
+console.log(Parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+root.render(Parent)
