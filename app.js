f3f905a47d3a8197dc1292a32446519d73004aba
Hasan Shaikh <35498957+shasan419@users.noreply.github.com>
2025-04-11 02:31:58 +0530

    episode-02

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
 // const heading = React.createElement("h1", {id:"heading"}, "Hello World from React!");
 const Parent = React.createElement("div", {id:"parent"}, [
     React.createElement("div",{id:"child"},[
-        React.createElement("h1", {}, "Hello World from React!"),
+        React.createElement("h1", {}, "Hello World from React! h1"),
         React.createElement("h2", {}, "Hello World from React!"),
     ]),
     React.createElement("div",{id:"child"},[
@@ -9,7 +11,7 @@ const Parent = React.createElement("div", {id:"parent"}, [
         React.createElement("h2", {}, "Hello World from React!"),
     ])
 ])
-console.log(Parent)
+console.log(Parent) //object
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
-root.render(Parent)
+root.render(Parent) //conver react obj to html elem and render it to dom
