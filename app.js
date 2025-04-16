351b26f34fe7af863a31b84070e23b70d43f8591
Hasan Shaikh <35498957+shasan419@users.noreply.github.com>
2025-04-17 00:30:40 +0530

    episode-03

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
-// const heading = React.createElement("h1", {id:"heading"}, "Hello World from React!");
-const Parent = React.createElement("div", {id:"parent"}, [
-    React.createElement("div",{id:"child"},[
-        React.createElement("h1", {}, "Hello World from React! h1"),
-        React.createElement("h2", {}, "Hello World from React!"),
-    ]),
-    React.createElement("div",{id:"child"},[
-        React.createElement("h1", {}, "Hello World from React!"),
-        React.createElement("h2", {}, "Hello World from React!"),
-    ])
-])
-console.log(Parent) //object
+import ReactDOM from "react-dom/client";
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World from React!"
+// );
+// console.log(heading); //object
+// const jsxHeading = (
+//   <h1 id="heading" className="head" tabIndex="1">
+//     Hello World from React!
+//   </h1>
+// );
+const Title = () => {
+  return (
+    <h1 id="heading" className="head" tabIndex="1">
+      Hello World from React!
+    </h1>
+  )
+}
+const Heading = () => (
+  <div>
+    {/* {jsxHeading} {console.log("here")}// we can use javascript inside JSX using {} */}
+    {/* {Title()} */}
+    <Title/>
+    {/* <Title></Title> */}
+  </div>
+);
+// console.log(jsxHeading); //object
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
-root.render(Parent) //conver react obj to html elem and render it to dom
+// root.render(jsxHeading); //conver react obj to html elem and render it to dom
+root.render(<Heading/>)
